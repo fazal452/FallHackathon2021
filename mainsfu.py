62e68c6c10352f5b7894fee75fdf42e6a9c6b957
fazal452 <fazal452@gmail.com>
2021-11-27 11:39:43 -0800

    Function opens up browser

--- mainsfu.py
+++ mainsfu.py
@@ -1,3 +1,8 @@
-import selenium
-import time
+#!/usr/bin/env python
 from selenium import webdriver
+
+
+
+#Open up SFU Website
+browser = webdriver.Chrome()
+browser.get('https://www.lib.sfu.ca/facilities/rooms-spaces/group-study-rooms')
