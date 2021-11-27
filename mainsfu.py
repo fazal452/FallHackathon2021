d1c09dde89834fb5a8b62566a49a217ff7132951
fazal452 <fazal452@gmail.com>
2021-11-27 12:32:10 -0800

    added safe start

--- mainsfu.py
+++ mainsfu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from selenium import webdriver
+import time
 
 
 
@@ -21,7 +22,7 @@ class booking:
         #day goes from 0 - 6
         self.day = day
 
-        #time goes form 8.5 to 23.5 increment of .5
+        #time goes form 8.5 to 22.5 increment of .5
         self.timeStart = timeStart
 
         #duration is in increament of .5 all the way to 2
@@ -32,8 +33,15 @@ class booking:
 
 
 
+#Authenticate User Firstly
 
-
-#Open up SFU Website
 browser = webdriver.Chrome()
-browser.get('http://roombookings.lib.sfu.ca/studyrooms/day.php?area=1')
+browser.get('https://cas.sfu.ca/cas/login?method=browser%20=%20webdriver.Chrome()%20browser.get(%27http://roombrowser%20=%20webdriver.Chrome()%20browser.get(%27httpbrowser%20=%20webdriver.Chrome()%20browser.get(%27http://roombookings.lib.sfu.ca/studyrooms/day.php?area=1%27)://roombookings.lib.sfu.ca/studyrooms/day.php?area=1%27)bookings.lib.sfu.ca/studyrooms/day.php?area=1%27)POST&service=https://sims.erp.sfu.ca/psp/csprd/EMPLOYEE/SA/s/WEBLIB_SFU.ISCRIPT1.FieldFormula.IScript_Home')
+
+start = False
+
+start = input("enter 1 to start the program: ")
+
+if start:
+    #Open up SFU Website
+    browser.get('http://roombookings.lib.sfu.ca/studyrooms/day.php?area=1')
