fc172698684916392cc0715d862759f812be8333
fazal452 <fazal452@gmail.com>
2021-11-27 15:02:06 -0800

    Can book rooms, errors in messages

--- mainsfu.py
+++ mainsfu.py
@@ -29,43 +29,72 @@ class booking:
 
 
 
-#book1 = booking(0,7,60)
-
-#
-# #Authenticate User Firstly
-# browser = webdriver.Chrome()
-# browser.get('https://cas.sfu.ca/cas/login?method=browser%20=%20webdriver.Chrome()%20browser.get(%27http://roombrowser%20=%20webdriver.Chrome()%20browser.get(%27httpbrowser%20=%20webdriver.Chrome()%20browser.get(%27http://roombookings.lib.sfu.ca/studyrooms/day.php?area=1%27)://roombookings.lib.sfu.ca/studyrooms/day.php?area=1%27)bookings.lib.sfu.ca/studyrooms/day.php?area=1%27)POST&service=https://sims.erp.sfu.ca/psp/csprd/EMPLOYEE/SA/s/WEBLIB_SFU.ISCRIPT1.FieldFormula.IScript_Home')
-#
-# start = False
-#
-# start = input("enter 1 to start the program: ")
-#
-# if start:
-#     #Open up SFU Website
-#     browser.get('http://roombookings.lib.sfu.ca/studyrooms/day.php?area=1')
-
-
-testBook = booking(2021,11,27,20,0,28)
-
-listBookings = [booking(2021,11,27,20,0,28),booking(2021,11,28,20,0,28),booking(2021,11,29,20,0,28),booking(2021,11,30,20,0,28)]
-
+#Authenticate User Firstly
 driver = webdriver.Chrome()
+driver.get('https://cas.sfu.ca/cas/login?method=POST&service=https://sims.erp.sfu.ca/psp/csprd/EMPLOYEE/SA/s/WEBLIB_SFU.ISCRIPT1.FieldFormula.IScript_Home')
 
-for i in listBookings:
+start = False
+
+##Log User in
+username = driver.find_element_by_name("username")
+password = driver.find_element_by_name("password")
+submit = driver.find_element_by_name("submit")
+
+username.send_keys("fra22")
+password.send_keys("")
+
+submit.click()
+
+
+
+start = input("enter 1 to start the program: ")
+
+if start:
+    #Open up SFU Website
+    driver.get('http://roombookings.lib.sfu.ca/studyrooms/day.php?area=1')
+
+
+# year, month, day, hour, minute, room
+listBookings = [booking(2021,12,1,20,0,28),booking(2021,12,2,20,0,28),booking(2021,12,3,20,0,28),booking(2021,12,4,20,0,28),booking(2021,12,5,20,0,28)]
+
+
+for testBook in listBookings:
 
     bookID = "http://roombookings.lib.sfu.ca/studyrooms/add/"+"edit_entry.php?area=1&room=" + str(testBook.room) +"&hour=" + str(testBook.hour)+"&minute="+str(testBook.minute)+"&year="+ str(testBook.year) +"&month="+str(testBook.month)+"&day=" + str(testBook.day)
 
+
     #go to booking step
+
     driver.get(bookID)
 
-    sessionName = driver.find_element_by_name("name")
+    time.sleep(2)
 
-    sessionName.send_keys("some text")
+    try:
+        sessionName = driver.find_element_by_name("name")
 
-    timeDuration = driver.find_element_by_name("duration")
+        sessionName.send_keys("some text")
 
-    saveButton = driver.find_element_by_name("save_button")
+        timeDuration = driver.find_element_by_name("duration")
 
-    saveButton.click()
+        saveButton = driver.find_element_by_name("save_button")
+
+        saveButton.click()
+        time.sleep(2)
+
+        try:
+            rep = (driver.find_element_by_xpath('/html/body/h2').text)
+
+        except:
+            conflict = "null"
+
+        if "conflict" in rep.lower():
+            print("couldn't book")
+
+        else:
+            print("success")
+
+    except:
+        print("failed to book")
+
 
 
